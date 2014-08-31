645bdf7b00907f1cf51a00e77417cf3fd5641110
jzhang <jeffrey_zhang@berkeley.edu>
2014-08-31 13:10:47 -0700

    commiting

--- public/js/main.js
+++ public/js/main.js
@@ -23,12 +23,63 @@ require(['angular', './controllers', './directives', './filters', './services', 
 
     // Declare app level module which depends on filters, and services
 
-    angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
-      config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+    angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute'])
+      .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+        var photosources = [];
+
+
+        // $http.get('https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=06210d82421e04059a9a3c344e9a3932&user_id=91611553@N05&page=1&per_page=200&format=json&nojsoncallback=1').success(function(data) {
+        //     var img = data.photos.photo;
+        //     for (var i = 1; i < img.length; i++) {
+        //         photosources.push({
+        //             src: 'https://farm' + img[i].farm + '.staticflickr.com/' + img[i].server + '/' + img[i].id + '_' + img[i].secret + '.jpg'
+        //         });
+            
+        //     }
+        // }).error(function(){
+        //     console.log("flickr error");
+        // });
+    
+        $http.get('http://graph.facebook.com/305442362898377/photos?fields=images&limit=999').success(function(data) {
+            var img = data.data;
+            for (var j=img.length-1; j >= 0; j--) {
+                var json = {}
+                for (var i=0; i < img[j].images.length; i++) {         
+                    if ((img[j].images[i].width == 2048) || (img[j].images[i].height == 2048)) {
+                        json['link'] = img[j].images[i].source;
+                    }
+                    if ((img[j].images[i].width == 320) || (img[j].images[i].height == 320)) {
+                        json['src'] = img[j].images[i].source;
+                    }
+                    if (img[j].images[0].width > img[j].images[0].height) {
+                        json['orient'] = 'landscape';
+                    } else {
+                        json['orient'] = 'portrait';
+                    }
+                }
+                console.log(json);
+                photosources.push(json);
+            }
+        }).error(function(){
+            console.log("shoot")
+        });
+        $scope.photos = photosources;
+
+        $scope._Index = 0;
+
+        $scope.isActive = function(index) {
+            return $scope._Index === index;
+        };
+
+        $scope.showPhoto = function(index) {
+            $scope._Index = index;
+        };
+
+            
+
+
       }]);
+
 
     angular.bootstrap(document, ['myApp']);
 
